2a266f9e5ed08978ec2e53d6e816c04d017c9f75
Layal Singh <layal.singhkolkata@gmail.com>
2024-10-29 22:12:55 -0400

    plane, sphere

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -11,14 +12,32 @@ const camera = new THREE.PerspectiveCamera(75,
 0.1,
 1000);
 
-const axeshelper = new THREE.AxesHelper(3);
+const orbit = new OrbitControls(camera, renderer.domElement);
+const axeshelper = new THREE.AxesHelper(5);
 scene.add(axeshelper);
-camera.position.set(0, 2, 5);
+camera.position.set(-10, 30, 30);
+orbit.update();
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const cube = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(cube);
+
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+plane.rotation.x = -0.5 * Math.PI;
+
+const sphereGeometry = new THREE.SphereGeometry(4,50,50);
+const sphereMaterial = new THREE.MeshBasicMaterial({color: 0x0000ff,
+    wireframe: false});
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphere);
+sphere.position.set(10,10,3);
+
+const gridhelper = new THREE.GridHelper(30);
+scene.add(gridhelper);
 
 function animate(time) {
     cube.rotation.x = time/1000;
