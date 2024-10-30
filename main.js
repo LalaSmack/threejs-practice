c923f59582a64ff598c4311abd3a79d745613f89
Layal Singh <layal.singhkolkata@gmail.com>
2024-10-29 21:46:25 -0400

    rotating cube

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0x19F692 );
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const scene = new THREE.Scene();
 
-camera.position.z = 5;
+const camera = new THREE.PerspectiveCamera(75,
+    window.innerWidth/window.innerHeight,
+0.1,
+1000);
 
-function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+const axeshelper = new THREE.AxesHelper(3);
+scene.add(axeshelper);
+camera.position.set(0, 2, 5);
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(cube);
+
+function animate(time) {
+    cube.rotation.x = time/1000;
+    cube.rotation.y = time/1000;
+    renderer.render(scene, camera);
 }
-renderer.setAnimationLoop( animate );
+renderer.setAnimationLoop(animate);
